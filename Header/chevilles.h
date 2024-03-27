969f7be909c386d22382f6e123aa126509b221c1
Diallo <tanoudialo04@gmail.com>
2024-03-27 22:22:52 +0100

    Ajout de la fonctionalité cheville

---
+++ Header/chevilles.h
@@ -0,0 +1,11 @@
+#ifndef __CHEVILLE_
+#define __CHEVILLE_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int brocheNoir(char ser[], char sequenc[]);
+
+int brocheBlanche(char ser[], char sequence[]);
+
+#endif
---
+++ Src/chevilles.c
@@ -0,0 +1,28 @@
+
+#include "../Header/chevilles.h"
+
+
+// Nombres de couleurs correctes ET bien placées 
+int brocheNoir(char ser[], char sequenc[]){
+    int Noirs = 0;
+    int i = 0;
+    while(i < 4){
+        if(ser[i] == sequenc[i]){
+            Noirs++;
+        }
+        i++;
+    }
+    return Noirs;
+}
+
+// Nombre de couleurs présentes dans la séquence mais mal placées
+int brocheBlanche(char ser[], char sequence[]){
+    int Blanches = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(ser[i]  == sequence[j] && i != j)
+                Blanches++;
+        }
+    }
+    return Blanches;
+}
