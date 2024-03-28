2703768e4f7f5f34cca343bad3870c989dfe4ad0
Unknow <mon_adresse@gmail.com>
2024-03-28 18:17:07 +0100

    Ajout de header de game après modification de conflit

--- Header/game.h
+++ Header/game.h
@@ -2,11 +2,6 @@
 #define __GAME_H__
 
 #include<stdbool.h>
-#include<stdio.h>
-
-bool game(char secret_sequence[], char user_sequence[]);
-
-
 #include"../Header/saisieUser.h"
 #include"../Header/chevilles.h"
 #include"../Header/chargement_sequence.h"
