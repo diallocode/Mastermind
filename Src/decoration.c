67c17daafd7e3c67a13243200f7f894cfeb71f3a
Diallo <tanoudialo04@gmail.com>
2024-03-28 17:01:40 +0100

    Inclusion de la fonctionnalité Decoration

---
+++ Src/decoration.c
@@ -0,0 +1,56 @@
+#include "../Header/decoration.h"
+
+// Sans couleur
+void decoreApp1(){
+    printf("M     M  AAAAA  SSSSS  TTTTT  EEEEE  RRRR   M   M  III  N   N  DDDD\n");
+    printf("MM   MM  A   A  S        T    E      R   R  MM MM   I   NN  N  D   D\n");
+    printf("M M M M  AAAAA  SSSSS    T    EEEE   RRRR   M M M   I   N N N  D   D\n");
+    printf("M  M  M  A   A      S    T    E      R R    M   M   I   N  NN  D   D\n");
+    printf("M     M  A   A  SSSSS    T    EEEEE  R  RR  M   M  III  N   N  DDDD\n");
+}
+
+// Avec Couleur
+void decoreApp2(){
+    // Début du texte en rouge
+    printf("\033[0;31m");
+    printf("M     M  AAAAA  SSSSS  TTTTT  EEEEE  RRRR   M   M  III  N   N  DDDD        ****   ***\n");
+    printf("\033[0;33m");
+    printf("MM   MM  A   A  S        T    E      R   R  MM MM   I   NN  N  D   D      *** ***  ***\n");
+    printf("\033[0;34m");
+    printf("M M M M  AAAAA  SSSSS    T    EEEE   RRRR   M M M   I   N N N  D   D     ***   *    ***\n");
+    printf("\033[0;36m");
+    printf("M  M  M  A   A      S    T    E      R R    M   M   I   N  NN  D   D     ***        ***\n");
+    printf("\033[0;32m");
+    printf("M     M  A   A  SSSSS    T    EEEEE  R  RR  M   M  III  N   N  DDDD       ***      ***\n");
+    printf("\033[0;31m");
+    printf("Developper par :                                                            ***   ***\n");
+    printf("Mamadou tanou Diallo                                                          *****\n");
+    printf("Ahmat Salet Mahamat                                                             *\n");
+    // Réinitialiser la couleur à la normale
+    printf("\033[0m");
+}
+
+void afficherCoeur() {
+    printf("\033[0;31m");
+    printf("   ***   ***\n");
+    printf(" *** ***  ***\n");
+    printf("***   *   ***\n");
+    printf("***       ***\n");
+    printf(" ***     ***\n");
+    printf("  ***   ***\n");
+    printf("    *****\n");
+    printf("     **\n");
+    printf("\033[0m");
+}
+
+// Le code des couleurs
+/* 
+    Noir: \033[0;30m
+    Rouge: \033[0;31m
+    Vert: \033[0;32m
+    Jaune: \033[0;33m
+    Bleu: \033[0;34m
+    Magenta: \033[0;35m
+    Cyan: \033[0;36m
+    Blanc: \033[0;37m
+*/
